f323153734e27f7b344d6584f99c1dd6b5e2c591
Lin Zehang <linzehang@odc.cmbchina.cn>
2018-10-28 21:51:06 +0800

    feature: add Counter Example

---
+++ src/Counter.js
@@ -0,0 +1,16 @@
+
+import React, { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      {count}
+      <button onClick={() => setCount(count+1)}>+</button>
+      <button onClick={() => setCount(count-1)}>-</button>
+    </div>
+  )
+}
+
+export default Counter
