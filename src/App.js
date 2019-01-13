ca2df5086c83daa8a7c454211b3e0a9e4c52dbf8
Lin Zehang <linzehang@odc.cmbchina.cn>
2019-01-13 16:47:51 +0800

    feature: update todo example with useReducer hooks

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-import React, { Component, Fragment } from 'react';
+import React from 'react';
 import './App.css';
 
-import Counter from './Counter'
 import Todo from './Todo'
 
-class App extends Component {
-  state = {
-    showTodo: true
-  }
-
-  toggleTodoVisble = () => {
-    this.setState((prevState) => {
-      return {
-        showTodo: !prevState.showTodo
-      }
-    })
-  }
-
-  render() {
-    return (
-      <Fragment >
-        <Counter />
-        <button onClick={this.toggleTodoVisble}>toggle todo</button>
-        {this.state.showTodo && <Todo />}
-      </Fragment >
-    )
-  }
+const App = () => {
+  return <Todo />
 }
 
 export default App;
--- src/Todo.js
+++ src/Todo.js
@@ -1,19 +1,48 @@
-import React from 'react'
-import { useInput, useOnMount, useOnUnmount } from 'react-hanger'
+import React, { useState, useReducer } from "react";
+import './Todo.css'
 
-function Todo() {
-  const newTodo = useInput('')
-  useOnMount(() => {
-    console.log('onMount')
-  })
-
-  useOnUnmount(() => {
-    console.log('onUnmount')
-  })
-
-  return (
-    <input value={newTodo.value} onChange={newTodo.onChange} />
-  )
+function todoReducer(state, action) {
+  switch (action.type) {
+    case "ADD":
+      return [...state, action.value];
+    case "REMOVE":
+      return state.filter(todo => todo !== action.value);
+    default:
+      return state;
+  }
 }
 
-export default Todo
+function TodoList() {
+  const [newTodo, updateTodo] = useState("");
+  const [todoList, dispatch] = useReducer(todoReducer, []);
+
+  const handleAdd = () => {
+    dispatch({ type: "ADD", value: newTodo });
+  };
+
+  const handleDelete = (value) => {
+    console.log('value', value)
+    dispatch({ type: "REMOVE", value });
+  };
+
+  const handleInputChange = (e) => {
+    updateTodo(e.target.value)
+  }
+
+  return (
+    <div>
+      <input value={newTodo} onChange={handleInputChange} />
+      <button onClick={handleAdd}>add</button>
+      <ul>
+        {todoList.map((todo, index) => (
+          <li key={index}>
+           <p className="todo">{todo}</p>
+           <button onClick={() => handleDelete(todo)}>&times;</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
