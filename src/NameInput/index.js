f551afa53580ccbb95696c13f0e73852cd3abc4c
Lin Zehang <linzehang@odc.cmbchina.cn>
2019-01-28 00:24:24 +0800

    feature: add NameInput

---
+++ src/NameInput/index.js
@@ -0,0 +1,26 @@
+import React, { useState, useRef } from 'react'
+
+function NameInput() {
+  let [name, setName] = useState("Nate");
+
+  let nameRef = useRef();
+
+  const submitButton = () => {
+    setName(nameRef.current.value);
+  };
+
+  return (
+    <div className="App">
+      <p>{name}</p>
+
+      <div>
+        <input ref={nameRef} type="text" />
+        <button type="button" onClick={submitButton}>
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default NameInput
