e5dedcac150686041e1001b0fac8d60afa25f3ac
Lin Zehang <linzehang@odc.cmbchina.cn>
2019-01-28 00:44:10 +0800

    feature: add CountDown

---
+++ src/CountDown/index.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react'
+
+const CountDown = () => {
+  const [count, setCount] = useState(10)
+
+  useEffect(() => {
+    setInterval(() => {
+      setCount(count - 1)
+    }, 1000)
+
+  }, [])
+
+  return (
+    <p>{count}</p>
+  )
+}
+
+export default CountDown
